f5169d94af129ee21c9dae85296d66277ec05ee1
liaochonglc <1342709488@qq.com>
2018-10-02 22:38:16 +0800

    加了个url重写

---
+++ IPTV/src/main/java/com/ido/iptv/common/config/UrlReWriterConfig.java
@@ -0,0 +1,9 @@
+package com.ido.iptv.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.tuckey.web.filters.urlrewrite.UrlRewriteFilter;
+
+@Configuration
+public class UrlReWriterConfig extends UrlRewriteFilter {
+
+}
